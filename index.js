baa6990dbf37fcc3c76eedad90a79c5cf232d129
Dunamis-001 <alex.nyamai@student.moringaschool.com>
2025-06-25 10:43:11 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+function writeCards(names, gift) {
+  
+  const messages = [];
+
+
+  for (let i = 0; i < names.length; i++) {
+    const name = names[i];
+    
+    const message = `Thank you, ${name}, for the wonderful ${gift} gift!`;
+
+    messages.push(message);
+  }
+
+
+  return messages;
+}
+
+
+function countDown(startNumber) {
+  for (let i = startNumber; i >= 0; i--) {
+    console.log(i);
+  }
+}
+
